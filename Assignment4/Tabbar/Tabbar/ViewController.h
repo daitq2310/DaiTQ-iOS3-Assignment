9d63b4a4082d843732f9129f7a9892aa0dccc8f3
Quang Đại <quangdai.trinh@gmail.com>
2016-04-09 10:38:19 +0700

    Added files via upload

---
+++ Assignment4/Tabbar/Tabbar/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Tabbar
+//
+//  Created by Quang Dai on 4/8/16.
+//  Copyright © 2016 Quang Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property UIViewController *viewController1;
+@property UIViewController *viewController2;
+@property UITabBarController *tabBarController;
+
+@end
+
---
+++ Assignment4/Tabbar/Tabbar/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  Tabbar
+//
+//  Created by Quang Dai on 4/8/16.
+//  Copyright © 2016 Quang Dai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.viewController1 = [[UIViewController alloc] init];
+    self.viewController1.view.backgroundColor = [UIColor redColor];
+    self.viewController1.tabBarItem.title = @"Controller 1";
+    
+    self.viewController2 = [[UIViewController alloc] init];
+    self.viewController2.view.backgroundColor = [UIColor blueColor];
+    self.viewController2.tabBarItem.title = @"Controller 2";
+    
+    self.tabBarController = [[UITabBarController alloc] init];
+    [self.tabBarController setViewControllers:@[self.viewController1, self.viewController2] animated:YES];
+    
+    [self.view addSubview:self.tabBarController.view];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
