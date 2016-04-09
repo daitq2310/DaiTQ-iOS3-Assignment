2f7cffe77fa1c4edf4ed7895749afbd6541efdc1
Quang Đại <quangdai.trinh@gmail.com>
2016-04-09 10:43:53 +0700

    Added files via upload

--- Assignment4/Tabbar/Tabbar/ViewController.m
+++ Assignment4/Tabbar/Tabbar/ViewController.m
@@ -18,13 +18,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.viewController1 = [[UIViewController alloc] init];
+    self.viewController2 = [[UIViewController alloc] init];
     self.viewController1.view.backgroundColor = [UIColor redColor];
     self.viewController1.tabBarItem.title = @"Controller 1";
-    
-    self.viewController2 = [[UIViewController alloc] init];
     self.viewController2.view.backgroundColor = [UIColor blueColor];
     self.viewController2.tabBarItem.title = @"Controller 2";
-    
     self.tabBarController = [[UITabBarController alloc] init];
     [self.tabBarController setViewControllers:@[self.viewController1, self.viewController2] animated:YES];
     
