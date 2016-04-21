802d3f11c0fd8acd42c0f39dcd6e37c4e473626b
Quang Đại <quangdai.trinh@gmail.com>
2016-04-21 21:48:53 +0700

    Added files via upload

--- Assignment7/OrientationAndShake/OrientationAndShake/ViewController.m
+++ Assignment7/OrientationAndShake/OrientationAndShake/ViewController.m
@@ -17,6 +17,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationChanged:) name:UIDeviceOrientationDidChangeNotification object:nil];
+    
+}
+
+#pragma mark - Detect device orientation
+- (void) orientationChanged : (NSNotification *) notification {
+    [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+    UIDeviceOrientation orientation = [[UIDevice currentDevice] orientation];
+    if (orientation == UIDeviceOrientationPortrait) {
+        NSLog(@"Portrait");
+        _lblDetectOrientationChanged.text = @"Portrait";
+    } else if (orientation == UIDeviceOrientationLandscapeLeft) {
+        NSLog(@"LandscapeLeft");
+        _lblDetectOrientationChanged.text = @"LandscapeLeft";
+        _lblDetectOrientationChanged.text = @"LandscapeLeft";
+    } else if (orientation == UIDeviceOrientationLandscapeRight) {
+        NSLog(@"LandscapeRight");
+        _lblDetectOrientationChanged.text = @"LandscapeRight";
+    } else if (orientation == UIDeviceOrientationPortraitUpsideDown){
+        NSLog(@"PortraitUpsideDown");
+        _lblDetectOrientationChanged.text = @"PortraitUpsideDown";
+    }
+}
+
+#pragma mark - Detect shake motion event
+- (BOOL)canBecomeFirstResponder{
+    return YES;
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [self becomeFirstResponder];
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (motion == UIEventSubtypeMotionShake){
+        NSLog(@"DEVICE SHAKED");
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"SHAKE DETECTED" message:@"Device Shaked" delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+        [alert show];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
